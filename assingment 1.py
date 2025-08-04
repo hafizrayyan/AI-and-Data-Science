472147af6e259dec5b9f018c2fa6b0469f1ee571
hafizrayyan <asifrayyam30@gmail.com>
2025-08-04 02:29:15 -0700

    uploading first assignment

---
+++ assingment 1.py
@@ -0,0 +1,91 @@
+
+#airthmatic operators
+a= 15
+y= 4
+print("Addition : a+y=" , a+y)
+print("Subtraction : a-y=" , a-y)
+print("Multiplication : a*y=" , a*y)
+print("Division : a/y=" , a/y)
+print("Floor division : a//y=" , a//y)
+print("Modulus : a%y=" , a%y)
+print("Exponentation : a**y=" , a**y)
+
+# Answer : floor division roundoff the value of quetient and modulus give us left over (reminder)
+
+# Airthmatic assimgment operators
+
+x = 10
+print ("Intial value of x:",x)
+x+=5
+print("After x+=5 :",x)
+x-=4
+print("After x-=4 :",x)
+x*=2
+print("After x*=2 :",x)
+x/=2
+print("After x/=2 :",x)
+
+# Answer : value of x is 11
+
+# Comparision operators
+
+a=7
+b=10
+
+print("a==b:",a==b)
+print("a!=b:",a!=b)
+print("a>b:",a>b)
+print("a<b:",a<b)
+print("a>=b:",a>=b)
+print("a<=b:",a<=b)
+
+# Answer : a!=b , a<b and a<=b are true
+
+# Logical operators
+
+x = "true"
+y = "false"
+print("x and y:",x and y)
+#false
+print("x or y:",x or y)
+# true
+print("not x:", not x)
+# false
+
+#membership operators
+
+institute= "Saylani Mass IT"
+
+print("'s' in institute:","s"in institute)
+print("'Mass' in institute:","Mass"in institute)
+print("'Saylani' not in institute:","Saylani"not in institute)
+
+# Identity operators
+
+a=5
+b=5
+c=1000
+
+print("a is b:", a is b)
+print("a is c:", a is c)
+print("c is not b:", c is not b)
+
+
+#bonus challenge
+
+username = input(" Enter your username :")
+password = input("Enter your password :")
+
+if username == "Talha" or username != "Talha" and password == "Axiom123 " or  password != "Axiom123" :
+             print("Login succesful " and "Invalid" )
+
+    
+
+
+
+
+
+
+
+
+
---
+++ student.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+student_nm = input("Enter your name : " )
+student_ag = int(input("Enter your age : " ))
+student_ins = input("Enter your institute name : " )
+student_cr = input("Enter your course name : " )
+print( "Name : {} \
+\nAge :{} \
+\nInstitute name :{} \
+\nCourse name :{} ".format(student_nm,student_ag,student_ins,student_cr))
+
+
+
+
+
+
+       
