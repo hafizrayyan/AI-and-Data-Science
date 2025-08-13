29f3e7f8205eef2a728e5efa3e81c29bdb5f975e
hafizrayyan <asifrayyan30@gmail.com>
2025-08-13 12:00:35 -0700

    Assignment 2 Part 2 Q 9.py

--- Assignment 2/Assignment 2 Part 2 Q 9.py
+++ Assignment 2/Assignment 2 Part 2 Q 9.py
@@ -1,11 +1,9 @@
 # part 2 Q 9
 passs= "Fifa"
 e = input("Enter the password :")
-print("wrong password")
-
 while e != passs :
        
         e = input("Enter the password :")
         print("wrong password")
-else:
-    print("Login successful")
+
+print("Login successful")
